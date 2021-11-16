b01de6d32bb45c614cb3d2ab829846458341fa7d
Soilhat <soilhatmohamed@gmail.com>
2021-11-16 20:26:44 +0100

    Start Sidebar

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Soilhat.github.io
-Official website for Soilhat MOHAMED
+Official website for Soilhat MOHAMED available on https://soilhat.github.io
--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Home from "./scenes/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <div className="container gutter-top">
+        <div className="row">
+          <aside className="col-12 col-md-12 col-xl-3">
+            <Sidebar/>
+          </aside>
+          <div className="col-12 col-md-12 col-xl-9">
+            <div className="box shadow pb-0">
+              <Routes>
+                <Route path="/"element={<Home />}/>
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
